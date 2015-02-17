e30bdd6d6d4e55926a0aa3bd784ebdd33bf61d94
rspaargaren <github@speijkers.demon.nl>
2015-02-17 22:15:21 +0100

    Update TEST_RFTRANSMIT.ino

--- TEST_RFTRANSMIT.ino
+++ TEST_RFTRANSMIT.ino
@@ -9,9 +9,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-//verstuur.sendPIR1(31,31);
-//verstuur.sendGeneric(31,15,true,111);
-//verstuur.sendGeneric(31,15,false,111);
+verstuur.sendPIR1(31,31);
+verstuur.sendGeneric(31,15,true,111);
+verstuur.sendGeneric(31,15,false,111);
 verstuur.sendDimmer1(12,12,true,true,10);
 verstuur.sendSwitch1(8,8,true,true);
 verstuur.sendContact1(5,5,true,true);
