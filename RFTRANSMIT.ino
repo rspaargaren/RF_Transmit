f21a2d9e4c6b6e0f3d864c2d9271a5ad3e826ac4
rspaargaren <github@speijkers.demon.nl>
2015-03-05 15:15:15 +0100

    Adding true/false to PIR
    Adding true/false value to PIR so it is matching the library file.

--- RFTRANSMIT.ino
+++ RFTRANSMIT.ino
@@ -9,7 +9,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-verstuur.sendPIR1(31,31);
+verstuur.sendPIR1(31,31,true); //updated the true/false value. RF_protocols is not using this value. It is always true if the
+// signal is received and switches off after the time out. I just included as it is part of the protocol. 
 verstuur.sendGeneric(31,15,true,111);
 verstuur.sendGeneric(31,15,false,111);
 verstuur.sendDimmer1(12,12,true,true,10);
